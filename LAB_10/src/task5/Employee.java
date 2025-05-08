6661497e579d13066f38557705072173ee25d961
hassanmemonexe <160129889+hassanmemonexe@users.noreply.github.com>
2025-05-08 11:52:21 +0500

    Update Employee.java

--- LAB_10/src/task5/Employee.java
+++ LAB_10/src/task5/Employee.java
@@ -3,6 +3,9 @@ package task5;
 public abstract class Employee {
     String name;
     int id;
-
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
     public abstract double calculateSalary();
 }
