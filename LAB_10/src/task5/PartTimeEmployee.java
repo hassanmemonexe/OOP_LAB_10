2bbd21450a9f45fe4744f064695d3aa98b750ecb
hassanmemonexe <160129889+hassanmemonexe@users.noreply.github.com>
2025-05-08 11:54:26 +0500

    Update PartTimeEmployee.java

--- LAB_10/src/task5/PartTimeEmployee.java
+++ LAB_10/src/task5/PartTimeEmployee.java
@@ -10,4 +10,7 @@ public class PartTimeEmployee extends Employee implements TaxPayer{
     public void payTax() {
         System.out.println("2% tax payed");
     }
+    public PartTimeEmployee(String name, int age){
+        super(name, age);
+    }
 }
